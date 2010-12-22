463ba4148b0fc62523f9c214abf3bd0c2ca6a37f
Denis Bykov <denis.bykov@cleancode.ru>
2010-12-23 00:22:16 +0300

    Generators updated

---
+++ Generator/AbstractGen.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GeneratorSubsystem;
+
+namespace GeneratorSubsystem
+{
+    public abstract class AbstractGen : IGen
+    {
+        public abstract int[] GenerateForDay();
+
+        public abstract double GetProbability(double x);
+
+        public abstract IEnumerable<double> GenerateSequence();
+
+        public virtual double[] GenerateN(int n)
+        {
+            return this.GenerateSequence().Take(n).ToArray();
+        }
+    }
+}
